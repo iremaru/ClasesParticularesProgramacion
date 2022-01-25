9f1e29f2d7faac3913cf3cea34d8460bd1c58637
anavanesa77 <anav.olivares@gmail.com>
2022-01-25 10:36:13 +0000

    Clases del martes 25

--- practicas.py
+++ practicas.py
@@ -1,23 +1,48 @@
-hero_age = 0
-name = "Vanesa"
+def calcular_nota(nombre:str, edad, nota):
+    if (nota > 5):
+        print("Está aprobado") 
+    elif (nombre == 'Bonifacio' and nota <8):
+        print("Está suspendido")
+    elif (edad > 18 and nota >= 4):
+        print("Está aprobado")
+    elif (nombre == 'Antonio' and nota >=3):
+        print("Está aprobado")
+    else:
+        print("Está suspendido")
+    
 
-def asignar_edad():
-    global hero_age 
-    hero_age += 1
+# alumnos normales
+print("/***************************************/")
+print("/*******ALUMNOS NORMALES****************/")
+print("[] Alumno Jesus, con 16 años y nota 6 debe ser aprobado")
+calcular_nota("Jesus", 16, 6)   # Aprobado
+print("[] Alumno María, con 16 años y nota 4 debe ser suspendida")
+calcular_nota("María", 16, 4)   # Suspe:ndido
+print("[] Alumno José, con 16 años y nota 9 debe ser aprobado")
+calcular_nota("José", 16, 9)    # Aprobado
+print("/***************************************/")
+print()
 
-def asignar_nuevo_nombre():
-    name = "Sofía"
-    return name
+print("/***************************************/")
+print("/*******ALUMNOS REPETIDORED****************/")
+print("Alumno Alfonso, con 19 años y nota 4 debe ser aprobado")
+calcular_nota("Alfonso", 19, 4)   # Aprobado
+print("Alumno Bruno, con 20 años y nota 3 debe ser suspendido")
+calcular_nota("Bruno", 40, 3)   # Suspenso
+print("Alumno Carlos, con 40 años y nota 2 debe ser suspendido")
+calcular_nota("Carlos", 40, 2)    # Suspenso
+print("/***************************************/")
+print()
 
-new_name = asignar_nuevo_nombre()
+print("/***************************************/")
+print("/*******ALUMNOS ESPECIALES****************/")
 
-asignar_edad()
-asignar_edad()
-asignar_edad()
-asignar_edad()
-asignar_edad()
-asignar_edad()
-
-print(name)
-print(hero_age)
-
+print("Alumno Antonio, con 16 años y nota 3 debe ser aprobado")
+calcular_nota("Antonio", 16, 3)   # Aprobado
+print("Alumno Antonio, con 23 años y nota 3 debe ser aprobado")
+calcular_nota("Antonio", 23, 3)   # Aprobado
+print("Alumno Bonifacio, con 16 años y nota 8 debe ser aprobado")
+calcular_nota("Bonifacio", 16, 8)    # Aprobado
+print("Alumno Bonifacio, con 40 años y nota 5 debe ser suspenso")
+calcular_nota("Bonifacio", 40, 5)    # Suspenso
+print("/***************************************/")
